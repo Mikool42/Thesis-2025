6156a4e6da651c15a0d48a0d61a10d503c737fbc
HallurKrist <55838486+HallurKrist@users.noreply.github.com>
2025-01-31 18:59:53 +0100

    Start on the programming side

--- Thesis 2025 Prototype/Assets/Scripts/PlayerMovement.cs
+++ Thesis 2025 Prototype/Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,42 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 10f;
 
+    [SerializeField] GameObject target;
+
+    [SerializeField] float forceAmount = 2f;
+
     Vector3 movement;
 
+    Vector3 moveVector = new Vector3(1f, 0, 0);
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         ProcessTranslation();
-
+        //ApplyMovementforce();
     }
 
     public void OnMove(InputValue value)
     {
         movement = value.Get<Vector3>();
+    }
+
+    public void OnFire()
+    {
+        Debug.Log("Fire");
+        Rigidbody targetRB = target.GetComponent<Rigidbody>();
+        if (targetRB != null)
+        {
+            Vector3 appliedForce = moveVector * forceAmount;
+            Debug.Log(appliedForce);
+            targetRB.AddForce(appliedForce, ForceMode.Impulse);
+        }
     }
 
     private void ProcessTranslation()
@@ -29,7 +53,24 @@ public class PlayerMovement : MonoBehaviour
         float zOffset = movement.z * controlSpeed * Time.deltaTime;
         float rawZPos = transform.localPosition.z + zOffset;
 
-        transform.localPosition = new Vector3(rawXPos, transform.localPosition.y, rawZPos);
+        Vector3 newPos = new Vector3(rawXPos, transform.localPosition.y, rawZPos);
+
+        if (newPos != transform.localPosition)
+        {
+            moveVector = newPos - transform.localPosition;
+            transform.rotation = Quaternion.LookRotation(moveVector);
+        }
+
+        transform.localPosition = newPos;
+
+    }
+
+    private void ApplyMovementforce()
+    { 
+        
+
+
+
     }
 
     
