71261320ab9731ff6af3ee115f455088ec717b99
HallurKrist <55838486+HallurKrist@users.noreply.github.com>
2025-02-06 17:51:48 +0100

    Sliding door first iteration
    Sliding door first iteration done

---
+++ Thesis 2025 Prototype/Assets/Scripts/SlidingDoorController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SlidingDoorController : MonoBehaviour
+{
+    [SerializeField] float closingForce = 5f;
+
+    [Tooltip("Don't cahnge, is a reference to the movable part of the door for the script.")]
+    [SerializeField] Rigidbody DoorBody;
+
+    private Vector3 _defaultDirectionVector = new Vector3(1, 0, 0);
+    private Vector3 currentDirectionVector;
+
+    void Start()
+    {
+        currentDirectionVector = transform.rotation * _defaultDirectionVector;
+        Debug.Log(currentDirectionVector);
+
+        if (Mathf.Abs(currentDirectionVector.x) < Mathf.Abs(currentDirectionVector.z))
+        {
+            DoorBody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+            currentDirectionVector = new Vector3(0, 0, currentDirectionVector.z);
+        }
+        else
+        {
+            DoorBody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+            currentDirectionVector = new Vector3(currentDirectionVector.x, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        DoorBody.AddForce(currentDirectionVector * closingForce, ForceMode.Force);
+
+
+    }
+}
