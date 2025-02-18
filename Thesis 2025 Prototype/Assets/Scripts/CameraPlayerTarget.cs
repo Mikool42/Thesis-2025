5d4508cce22cac835acca320b3e975616cb7be03
HallurKrist <55838486+HallurKrist@users.noreply.github.com>
2025-02-18 16:06:57 +0100

    Camera stuff and player spawing with it has been added

--- Thesis 2025 Prototype/Assets/Scripts/CameraPlayerTarget.cs
+++ Thesis 2025 Prototype/Assets/Scripts/CameraPlayerTarget.cs
@@ -17,4 +17,19 @@ public class CameraPlayerTarget : MonoBehaviour
     {
         
     }
+
+    public void OnPlayerJoined()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        targetGroup.Targets.Clear();
+
+        foreach (GameObject _player in players)
+        {
+            targetGroup.AddMember(_player.transform, 1, 1);
+        }
+
+        Debug.Log(players.Length);
+
+    }
 }
