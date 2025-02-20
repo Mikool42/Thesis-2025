f44e4f708abb61ca6809b5054078d9a1a8f5422e
HallurKrist <55838486+HallurKrist@users.noreply.github.com>
2025-02-20 12:42:01 +0100

    Jumping first iteration done

---
+++ Thesis 2025 Prototype/Assets/Scripts/FloorDetection.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class FloorDetection : MonoBehaviour
+{
+    [SerializeField] private bool isGrounded = false;
+    private GameObject currentGroundObject = null; // used to hinder player becoming not grounded while grounded
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string objTag = other.gameObject.tag;
+
+        if (objTag == "Ground" || objTag == "MovableObject")
+        {
+            isGrounded = true;
+            currentGroundObject = other.gameObject;
+        }
+
+        Debug.Log(objTag);
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        string objTag = other.gameObject.tag;
+
+        if (objTag == "Ground" || objTag == "MovableObject" && currentGroundObject == other.gameObject)
+        {
+            isGrounded = false;
+            currentGroundObject = null;
+        }
+
+        Debug.Log(objTag);
+    }
+
+    public bool GetIsGrounded() { return isGrounded; }
+}
--- Thesis 2025 Prototype/Assets/Scripts/PlayerMovement.cs
+++ Thesis 2025 Prototype/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,14 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Tooltip("Reference to floor detection script")]
+    [SerializeField] private FloorDetection FDScript;
+
     [Tooltip("Player movement speed")]
     [SerializeField] float moveSpeed = 5f;
+
+    [Tooltip("Jump Force")]
+    [SerializeField] float jumpForce = 5f;
 
     private Rigidbody rb;
     private Vector3 m_Move;
@@ -36,5 +42,13 @@ public class PlayerMovement : MonoBehaviour
         var move = Quaternion.Euler(0, 0, 0) * direction;
 
         rb.Move(transform.position + (Vector3.Normalize(direction) * scaledMoveSpeed), Quaternion.LookRotation(direction, Vector3.up));
+    }
+    
+    public void OnJump()
+    {
+        if (FDScript != null && FDScript.GetIsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
