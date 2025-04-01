3408f745116e74454d7c819d2c64f23713c5e790
HallurKrist <55838486+HallurKrist@users.noreply.github.com>
2025-04-01 18:54:47 +0200

    Outline first draft complete

--- Thesis 2025 Prototype/Assets/Scripts/MovableObjectTargetColorSwitch.cs
+++ Thesis 2025 Prototype/Assets/Scripts/MovableObjectTargetColorSwitch.cs
@@ -5,8 +5,9 @@ public class MovableObjectTargetColorSwitch : MonoBehaviour
     [Tooltip("Default Movable Object Material Material")]
     [SerializeField] private Material material1;
 
-    [Tooltip("When targeted Material")]
-    [SerializeField] private Material material2;
+    [Tooltip("Outline material if applicable")]
+    [SerializeField] private Material outline;
+    [SerializeField] private bool hasOutline = false;
     
     [Tooltip("MeshRenderer to cahnge color on, if not assigned will use default MeshRenderer")]
     [SerializeField] MeshRenderer presetMeshRenderer;
@@ -24,7 +25,20 @@ public class MovableObjectTargetColorSwitch : MonoBehaviour
         {
             mr = GetComponent<MeshRenderer>();
         }
-        mr.material = material1;
+
+        if (hasOutline)
+        {
+            Material[] matArray = mr.materials;
+            matArray[0] = material1;
+            matArray[1] = outline;
+            mr.materials = matArray;
+        }
+        else
+        {
+            mr.material = material1;
+        }
+
+        
     }
 
     public void SetAsTarget(bool isTarget, PlayerAbilityBehaviour.AbilityType abilityType)
@@ -32,10 +46,12 @@ public class MovableObjectTargetColorSwitch : MonoBehaviour
         if (isTarget)
         {
             SetMaterialBool(abilityType, 1);
+            if (hasOutline) SetOutlineBool(1);
         }
         else
         {
             SetMaterialBool(abilityType, 0);
+            if (hasOutline) SetOutlineBool(0);
         }
     }
 
@@ -48,6 +64,22 @@ public class MovableObjectTargetColorSwitch : MonoBehaviour
         else if (abilityType == PlayerAbilityBehaviour.AbilityType.PULL)
         {
             mr.material.SetInt("_PullTargeting", _bool);
+        }
+    }
+
+    private void SetOutlineBool(int _bool)
+    {
+        Material[] matArray = mr.materials;
+        if (_bool == 0)
+        {
+            if (matArray[0].GetInt("_PullTargeting") == 0 && matArray[0].GetInt("_PushTargeting") == 0)
+            {
+                matArray[1].SetInt("_Outline", _bool);
+            }
+        }
+        else
+        {
+            matArray[1].SetInt("_Outline", _bool);
         }
     }
 }
