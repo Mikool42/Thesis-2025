1be063679670f8ee902b07fb19ee0ee1750bf506
HallurKrist <55838486+HallurKrist@users.noreply.github.com>
2025-05-01 13:19:27 +0200

    Update FloorDetection.cs
    Fixed most issues I found

--- Thesis 2025 Prototype/Assets/Scripts/Player/FloorDetection.cs
+++ Thesis 2025 Prototype/Assets/Scripts/Player/FloorDetection.cs
@@ -1,21 +1,32 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class FloorDetection : MonoBehaviour
 {
     [SerializeField] private bool isGrounded = false;
-    private GameObject currentGroundObject = null; // used to hinder player becoming not grounded while grounded
+    private List<GameObject> currentGroundObjects = new List<GameObject>(); // used to hinder player becoming not grounded while grounded
 
     private void OnTriggerEnter(Collider other)
     {
-        string objTag = other.gameObject.tag;
+        /*string objTag = other.gameObject.tag;
 
         if (objTag == "Ground" || objTag == "MovableObject")
         {
-            isGrounded = true;
-            currentGroundObject = other.gameObject;
-        }
+        }*/
+        isGrounded = true;
+        currentGroundObjects.Add(other.gameObject);
 
         //Debug.Log(objTag);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        currentGroundObjects.Remove(other.gameObject);
+
+        if (currentGroundObjects.Count == 0 )
+        {
+            isGrounded = false;
+        }
     }
     
     /*private void OnTriggerExit(Collider other)
