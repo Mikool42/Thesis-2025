5e68bc5ce9beaf59b239489931f36a201f353f43
HallurKrist <halh@itu.dk>
2026-04-19 14:44:05 +0200

    Added new movable object script and player interaction with it

---
+++ Thesis 2025 Prototype/Assets/Scripts/Player/Player_refactor/NewMovableObject.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class NewMovableObject : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [Tooltip("How much force to use for level one")]
+    [SerializeField] float forceAmount_L1 = 2f;
+    [Tooltip("How much force to use for level two")]
+    [SerializeField] float forceAmount_L2 = 5f;
+    [Tooltip("How much force to use for level three")]
+    [SerializeField] float forceAmount_L3 = 10f;
+
+    private float appliedForceAmount = -1;
+    private Vector3 appliedForceDir = Vector3.zero;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rb != null && appliedForceAmount != -1 && appliedForceDir != Vector3.zero)
+        {
+            rb.AddForce(appliedForceAmount * appliedForceDir);
+
+            appliedForceAmount = -1;
+            appliedForceDir = Vector3.zero;
+        }
+    }
+
+    public void ApplyForceToObject(Vector3 forceDirection, int forceLevel)
+    {
+        if (forceLevel == 0) appliedForceAmount = forceAmount_L1;
+        else if (forceLevel == 1) appliedForceAmount = forceAmount_L2;
+        else if (forceLevel == 2) appliedForceAmount = forceAmount_L3;
+        else appliedForceAmount = -1;
+
+        appliedForceDir = forceDirection;
+    }
+}
